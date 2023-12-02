eb25ae8ddaf148f05e5aaeb10f14c2b3f7a86ce9
Richard Bartoš <ricci21@google.com>
2023-12-02 23:29:49 +0100

    Second star. Wow.

--- day02.py
+++ day02.py
@@ -52,6 +52,45 @@ def find_possible_games(data):
     return count
 
 
+def find_sum_power(data):
+    """
+    >>> find_sum_power(["Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green",
+    ... "Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue",
+    ... "Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red",
+    ... "Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red",
+    ... "Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green"])
+    2286
+    """
+
+    count = 0
+    edited_data = data
+
+    for item in edited_data:
+        max_throw_red = 0
+        max_throw_green = 0
+        max_throw_blue = 0
+
+        game = item.split(": ")
+        game[0] = ''.join(filter(str.isdigit, game[0]))
+        game[1] = game[1].split("; ")
+        # print(item)
+
+        for game_throw in game[1]:
+            game_throw = game_throw.split(", ")
+
+            for single_cubes_throw in game_throw:
+                if str(single_cubes_throw).find("red") != -1 and max_throw_red < int(''.join(filter(str.isdigit, single_cubes_throw))):
+                    max_throw_red = int(''.join(filter(str.isdigit, single_cubes_throw)))
+                elif str(single_cubes_throw).find("green") != -1 and max_throw_green < int(''.join(filter(str.isdigit, single_cubes_throw))):
+                    max_throw_green = int(''.join(filter(str.isdigit, single_cubes_throw)))
+                elif str(single_cubes_throw).find("blue") != -1 and max_throw_blue < int(''.join(filter(str.isdigit, single_cubes_throw))):
+                    max_throw_blue = int(''.join(filter(str.isdigit, single_cubes_throw)))
+
+        count += max_throw_red * max_throw_green * max_throw_blue
+
+    return count
+
+
 if __name__ == "__main__":
     with open(os.path.join(os.path.dirname(__file__), 'day02.txt')) as fh:
         for line in fh.readlines():
@@ -59,3 +98,4 @@ if __name__ == "__main__":
             data_input.append(line)
 
     print("Sum of all IDs of possible games: " + str(find_possible_games(data_input)))
+    print("Sum of power of minimum number of cubes: " + str(find_sum_power(data_input)))
